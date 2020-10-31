ba521dd2ff8ad72b1dd10d8cd55cf6af5ba2503b
cristianetaniguti <chaytaniguti@gmail.com>
2020-10-31 10:20:19 -0300

    track

--- 4biotec/4biotec.R
+++ 4biotec/4biotec.R
@@ -10,19 +10,19 @@ media_cultivar_ordenada <- sort(media_cultivar)
 
 desordem <- match(media_cultivar, media_cultivar_ordenada)
 
-ligados <- matrix(c(sample(c(rep(0,17), rep(1,3))),
+ligados <- matrix(c(sample(c(rep(0,19), rep(1,1))),
+                    sample(c(rep(0,17), rep(1,3))),
                     sample(c(rep(0,15), rep(1,5))),
-                    sample(c(rep(0,12), rep(1,8))),
                     sample(c(rep(0,13), rep(1,7))),
                     sample(c(rep(0,10), rep(1,10))),
-                    sample(c(rep(0,10), rep(1,7), rep(2,3))),
-                    sample(c(rep(0,8), rep(1,7), rep(2,5))),
-                    sample(c(rep(0,6), rep(1,4), rep(2,10))),
-                    sample(c(rep(1,9), rep(2,11))),
-                    sample(c(rep(1,5), rep(2,15)))),
+                    sample(c(rep(0,10), rep(1,10))),
+                    sample(c(rep(0,6), rep(1,14))),
+                    sample(c(rep(0,4), rep(1,16))),
+                    sample(c(rep(0,3), rep(1,17))),
+                    sample(c(rep(0,1), rep(1,19)))),
                   nrow = 20, byrow = T)
 
-nao_ligados <- matrix(sample(c(rep(2,97), rep(1,100), rep(0,103))), nrow = 20)
+nao_ligados <- matrix(sample(c(rep(1,147), rep(0,153))), nrow = 20)
 
 
 genotipos <- matrix(c(nao_ligados[,1:5], ligados[,1:5], nao_ligados[,6:12], ligados[,6:10],nao_ligados[,13:15]), nrow = 20)
@@ -52,8 +52,8 @@ fenotipos <- read.csv("https://raw.githubusercontent.com/Cristianetaniguti/Works
 str(fenotipos)
 head(fenotipos)
 
-rownames(fenotipos) <- fenotipos$cultivar
-fenotipos <- fenotipos[,-c(1,2)]
+#rownames(fenotipos) <- fenotipos$cultivar
+#fenotipos <- fenotipos[,-c(1,2)]
 
 genotipos <- read.csv("https://raw.githubusercontent.com/Cristianetaniguti/Workshop_genetica_esalq/gh-pages/4biotec/genotipos.csv", 
                       stringsAsFactors = F)
@@ -72,15 +72,10 @@ genotipos <- as.matrix(genotipos)
 
 heatmap(genotipos, Colv = NA, Rowv = NA)
 
-geno.cores <- heat.colors(3)
+geno.cores <- heat.colors(2)
 
 heatmap(genotipos, Colv = NA, Rowv = NA,  col=geno.cores)
 head(genotipos)
-
-geno.cores <- rev(geno.cores)
-
-heatmap(genotipos, Colv = NA, Rowv = NA,  col=geno.cores)
-heatmap(genotipos, Colv = NA, Rowv = NA,  col=geno.cores, scale = "column")
 
 fenotipos_ordenado <- sort(fenotipos)
 
@@ -89,7 +84,8 @@ desordem <- match(fenotipos, fenotipos_ordenado)
 colfunc <- colorRampPalette(c("royalblue", "red"))
 cores <- colfunc(20)[desordem]
 
-heatmap(genotipos, Colv = NA, Rowv = NA, col=rev(heat.colors(3)), RowSideColors = cores, scale = "column")
+heatmap(genotipos, Colv = NA, Rowv = NA, col=heat.colors(2), RowSideColors = cores)
+head(genotipos)
 
 ordem <- match(fenotipos_ordenado, fenotipos)
 
